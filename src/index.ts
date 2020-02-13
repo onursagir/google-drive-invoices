601a8845bec9e0e530c6ca835f1d41d5117cc9ae
Mehmet Onur Sagir <onuur.s@gmail.com>
2020-02-13 20:36:43 +0100

    config module

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import yargs from 'yargs';
 import auth from './auth';
 import uploadModule from './upload';
+import configModule from './config';
 
 const args = yargs
+  .command(configModule)
   .middleware(auth)
   .command(uploadModule)
   .help()
