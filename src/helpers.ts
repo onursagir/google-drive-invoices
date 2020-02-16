2264b169245d53fa0fb42679f9983829e6f86594
Mehmet Onur Sagir <onuur.s@gmail.com>
2020-02-16 13:16:00 +0100

    move auth to helper

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,8 +1,7 @@
 import fs from 'fs';
 import path from 'path';
-import Listr from 'listr';
-import mimeTypes from 'mime-types';
-import { drive_v3 } from 'googleapis';
+import inquirer from 'inquirer';
+import { drive_v3, google } from 'googleapis';
 import { Credentials } from 'google-auth-library/build/src/auth/credentials';
 
 interface Config {
@@ -49,4 +48,53 @@ export const findOrCreateFolder: FindOrCreateFolder = async (drive, pathName) =>
   }
 
   return baseDir;
+};
+
+interface AuthorizeReturn {
+  drive: drive_v3.Drive;
+}
+
+export const authorize = async (): Promise<AuthorizeReturn> => {
+  const config = await getConfig();
+
+  try {
+    const oAuthClient = new google.auth.OAuth2(
+      config.clientId,
+      config.clientSecret,
+      'urn:ietf:wg:oauth:2.0:oob',
+    );
+
+    if (config.tokens) {
+      oAuthClient.setCredentials(config.tokens);
+    } else {
+      const authURL = oAuthClient.generateAuthUrl({
+        access_type: 'offline',
+        scope: ['https://www.googleapis.com/auth/drive'],
+      });
+
+      const { code } = await inquirer.prompt([
+        {
+          name: 'code',
+          message: `Please visit the following URL and enter the displayed code ${authURL}`,
+        },
+      ]);
+
+      const { tokens } = await oAuthClient.getToken(code);
+      oAuthClient.setCredentials(tokens);
+
+
+      await writeConfig({ ...config, tokens });
+    }
+
+    const drive = google.drive({ version: 'v3', auth: oAuthClient });
+
+    return { drive };
+  } catch (e) {
+    const configWithoutTokens = { ...config };
+    delete configWithoutTokens.tokens;
+
+    await writeConfig(configWithoutTokens);
+
+    throw e;
+  }
 };
